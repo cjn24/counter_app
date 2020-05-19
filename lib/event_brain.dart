9adbc6c8a1ac7043f8b9c3af224e8e69d705684a
cjn24 <neill_ruiz@hotmail.com>
2020-05-19 11:00:18 +0200

    19/05

--- lib/event_brain.dart
+++ lib/event_brain.dart
@@ -3,21 +3,20 @@ import 'package:flutter/material.dart';
 
 class EventBrain{
   List<Event> eventBank = [
-    Event(0,	'Warmup',	false, 'highBeep.mp3', Colors.cyan),
-   // Event(17,	'Warmup',	false, 'pipBeep.mp3',	Colors.cyan),
-   // Event(18,	'Warmup',	false, 'pipBeep.mp3',	Colors.cyan),
-   // Event(19,	'Warmup',	false, 'pipBeep.mp3',	Colors.cyan),
-    Event(20,	'Interval 1 High',true, 'highBeep.mp3', Colors.red),
-   // Event(27,	'Interval 1 High',false, 'pipBeep.mp3',	Colors.red),
-   // Event(28,	'Interval 1 High',false, 'pipBeep.mp3',	Colors.red),
-   // Event(29,	'Interval 1 High',false,  'pipBeep.mp3',	Colors.red),
-    Event(30,	'Interval 1 Low', true, 'lowBeep.mp3',	Colors.green),
-   // Event(47,	'Interval 1 Low', false, 'pipBeep.mp3',	Colors.green),
-   // Event(48,	'Interval 1 Low', false, 'pipBeep.mp3',	Colors.green),
-   // Event(49,	'Interval 1 Low', false, 'pipBeep.mp3',	Colors.green),
-    Event(50,	'Cooldown',	true, 'lowBeep.mp3', Colors.orange),
-    Event(60, 'End', false, 'lowBeep.mp3', Colors.cyan),
-    Event(61, 'End', false, 'lowBeep.mp3', Colors.white),
+    Event(0,	20, 'Warmup',	'highBeep.mp3', Colors.cyan),
+   // Event(17,	'Warmup',	'pipBeep.mp3',	Colors.cyan),
+   // Event(18,	'Warmup',	'pipBeep.mp3',	Colors.cyan),
+   // Event(19,	'Warmup',	'pipBeep.mp3',	Colors.cyan),
+    Event(20,	10, 'Interval 1 High','highBeep.mp3', Colors.red),
+   // Event(27,	'Interval 1 High','pipBeep.mp3',	Colors.red),
+   // Event(28,	'Interval 1 High','pipBeep.mp3',	Colors.red),
+   // Event(29,	'Interval 1 High', 'pipBeep.mp3',	Colors.red),
+    Event(30,	20, 'Interval 1 Low', 'lowBeep.mp3',	Colors.green),
+   // Event(47,	'Interval 1 Low', 'pipBeep.mp3',	Colors.green),
+   // Event(48,	'Interval 1 Low', 'pipBeep.mp3',	Colors.green),
+   // Event(49,	'Interval 1 Low', 'pipBeep.mp3',	Colors.green),
+    Event(50,	10, 'Cooldown',	'lowBeep.mp3', Colors.orange),
+    Event(61, 1, 'End', 'lowBeep.mp3', Colors.cyan),
     ];
 
 }
