910084ca2c80c6a9b901b42b82e9c06205a6113c
cjn24 <neill_ruiz@hotmail.com>
2020-06-09 20:20:58 +0200

    09/06

---
+++ lib/interval.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+class Intervall {
+  String nameOfWorkout;
+  int lengthOfWorkout;
+  int numberOfIntervals;
+  int sequenceOfInterval;
+  int runTime;
+  int durationOfInterval;
+  String nameOfInterval;
+  String action;
+  dynamic lpibgColor;
+
+  Intervall (this.nameOfWorkout, this.lengthOfWorkout, this.numberOfIntervals, this.sequenceOfInterval, this.runTime, this.durationOfInterval, this.nameOfInterval, this.action, this.lpibgColor);
+
+  @override
+  String toString() {
+    return '{ ${this.nameOfWorkout}, ${this.lengthOfWorkout}, ${this.numberOfIntervals}, ${this.sequenceOfInterval}, ${this.runTime}, ${this.durationOfInterval}, ${this.nameOfInterval}, ${this.action}, ${this.lpibgColor} }';
+  }
+  // JSON mapping
+  Map toJson() => {
+    'name of workout': nameOfWorkout,
+    'lenght of workout': lengthOfWorkout,
+    'number of intervals': numberOfIntervals,
+    'sequence of interval': sequenceOfInterval,
+    'run time': runTime,
+    'duration of interval': durationOfInterval,
+    'name of interval': nameOfInterval,
+    'action': action,
+    'lpibg color': lpibgColor
+  };
+
+
+  // Methods (Functions)
+  void printValues(){
+    print(nameOfWorkout);
+    print(sequenceOfInterval);
+    }
+// Getters and Setters
+}
